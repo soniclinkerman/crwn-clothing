e4be321f5c15fca2632c390bb4db7b1871312566
Zay <Izpopsonic1@gmail.com>
2021-12-05 14:37:41 -0500

    Broken homepage into smaller components and finished styling menu item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./Homepage.component";
+import HomePage from "./pages/homepage/hompage.component";
 
 function App() {
   return (
